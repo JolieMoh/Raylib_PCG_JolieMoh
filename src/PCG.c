a93735a6b5274f6a77069d9ad5acf605249975a6
Jolie Moh <joliejingyi.moh@griffithuni.edu.au>
2026-04-03 15:19:02 +1000

    created a PCG map editor with Raylib
    with random grid, multiple files, file io and gui

---
+++ src/PCG.c
@@ -0,0 +1,144 @@
+#include "PCG.h"
+#include <stdio.h>
+
+
+// ============================================= 
+// PCG_CreateMap
+// ============================================= 
+void PCG_CreateMap(TileType _tileArray[MAP_ROWS][MAP_COLUMNS])
+{
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            _tileArray[y][x] = (TileType)GetRandomValue(0, TILE_COUNT - 1);
+        }
+    }
+}
+
+// ============================================= 
+// PCG_GetTileColor
+// ============================================= 
+Color PCG_GetTileColor(TileType tileType)
+{
+    switch (tileType) {
+    case TILE_TYPE_GRASS: return GRASS_COLOR;
+    case TILE_TYPE_ROCK: return ROCK_COLOR;
+    default: return UNKNOWN_COLOR;
+    }
+}
+
+// ============================================= 
+// PCG_DrawMap
+// ============================================= 
+void PCG_DrawMap(TileType _tileArray[MAP_ROWS][MAP_COLUMNS])
+{
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, PCG_GetTileColor(_tileArray[y][x]));
+        }
+    }
+}
+
+// ============================================= 
+// PCG_PrintMap
+// ============================================= 
+void PCG_PrintMap(TileType _tileArray[MAP_ROWS][MAP_COLUMNS])
+{
+    printf("\n-------Map Layout:--------\n");
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            if (_tileArray[y][x] == TILE_TYPE_GRASS) {
+                printf("%c", GRASS_CHAR);
+            }
+            else {
+                printf("%c", ROCK_CHAR);
+            }
+        }
+        printf("\n");
+    }
+    printf("--------------------------\n");
+}
+char GetTileChar(TileType tileType) {
+    switch (tileType) {
+    case TILE_TYPE_GRASS: return GRASS_CHAR;
+    case TILE_TYPE_ROCK: return ROCK_CHAR;
+    default: return '?';
+    }
+}
+
+void PCG_SaveMapData(TileType _tileArray[MAP_ROWS][MAP_COLUMNS], const char* _filename) {
+    FILE* file = fopen(_filename, "w"); // Write mode
+    if (file == NULL) return;
+
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            fputc(GetTileChar(_tileArray[y][x]), file);
+        }
+        fputc('\n', file);
+    }
+    fclose(file);
+}
+
+void PCG_LoadMapData(TileType _tileArray[MAP_ROWS][MAP_COLUMNS], const char* _filename) {
+    FILE* file = fopen(_filename, "r"); // Read mode
+    if (file == NULL) return;
+
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            int ch = fgetc(file);
+            // Skip invisible newline characters
+            while (ch == '\n' || ch == '\r') { ch = fgetc(file); }
+
+            if (ch == GRASS_CHAR) _tileArray[y][x] = TILE_TYPE_GRASS;
+            else if (ch == ROCK_CHAR) _tileArray[y][x] = TILE_TYPE_ROCK;
+        }
+    }
+    fclose(file);
+}
+// Img file save
+void PCG_SaveMapImage(TileType _tileArray[MAP_ROWS][MAP_COLUMNS], const char* filename) {
+    Image mapImage = GenImageColor(MAP_COLUMNS, MAP_ROWS, BLACK);
+
+    for (int y = 0; y < MAP_ROWS; y++) {
+        for (int x = 0; x < MAP_COLUMNS; x++) {
+            Color c = PCG_GetTileColor(_tileArray[y][x]);
+            ImageDrawPixel(&mapImage, x, y, c);
+        }
+    }
+
+    if (ExportImage(mapImage, filename)) {
+        printf("Image saved successfully!\n");
+    }
+    UnloadImage(mapImage); // Free heap memory
+}
+
+// Required to call Raylib gui buttons. Add this near the top of PCG.c
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h" 
+
+// ============================================= 
+// PCG_DrawGUI
+// ============================================= 
+void PCG_DrawGUI(TileType tileArray[MAP_ROWS][MAP_COLUMNS]) {
+    // Reset Button
+    if (GuiButton(RESET_BUTTON_BOUNDS, "Reset Map")) {
+        PCG_CreateMap(tileArray);
+    }
+
+    // Save Data Button
+    Rectangle saveRect = { BUTTON_X, BUTTON_Y - 70, BUTTON_WIDTH, BUTTON_HEIGHT };
+    if (GuiButton(saveRect, "Save Map Data")) {
+        PCG_SaveMapData(tileArray, MAP_TEXT_FILENAME);
+    }
+
+    // Load Data Button
+    Rectangle loadRect = { BUTTON_X, BUTTON_Y - 140, BUTTON_WIDTH, BUTTON_HEIGHT };
+    if (GuiButton(loadRect, "Load Map Data")) {
+        PCG_LoadMapData(tileArray, MAP_TEXT_FILENAME);
+    }
+
+    // Save Image Button
+    Rectangle imgRect = { BUTTON_X, BUTTON_Y - 210, BUTTON_WIDTH, BUTTON_HEIGHT };
+    if (GuiButton(imgRect, "Save Map PNG")) {
+        PCG_SaveMapImage(tileArray, MAP_IMAGE_FILENAME);
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,54 +1,42 @@
 /*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
+===============================================================================
+Construct Map Editor
+-------------------------------------------------------------------------------
+Author:      Griffith Film School
+Description: A simple tile-based map editor template using raylib for
+			 introductory programming students. Demonstrates 2D arrays,
+			 nested loops, and basic graphics rendering.
+===============================================================================
 */
 
 #include "raylib.h"
+#include "resource_dir.h"
+#include "PCG.h" // Import our new module
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-
-int main ()
+int main()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Construct Map Editor");
 
-	// Create the window and OpenGL context
-	InitWindow(800, 600, "Hello Raylib");
+    TileType tileArray[MAP_ROWS][MAP_COLUMNS] = { 0 };
+    PCG_CreateMap(tileArray);
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+    // Save text and image
+    PCG_SaveMapData(tileArray, MAP_TEXT_FILENAME);
+    PCG_SaveMapImage(tileArray, MAP_IMAGE_FILENAME);
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+        PCG_DrawMap(tileArray); // Function from PCG.c
+        PCG_DrawGUI(tileArray); 
+        DrawText("Construct Map Editor", 20, 20, 20, WHITE);
+        EndDrawing();
+        
+    }
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+    CloseWindow();
+    return 0;
 }
